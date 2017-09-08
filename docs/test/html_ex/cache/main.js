0587ff2f36b44007a0f673cad90bb4c65d90e9f1
babiron-modder <babiron2014@gmail.com>
2017-09-08 22:07:01 +0900

    add files

---
+++ docs/test/html_ex/cache/main.js
@@ -0,0 +1,25 @@
+﻿
+
+let area;
+
+document.addEventListener("DOMContentLoaded",function(){
+	
+	console.log(navigator.cookieEnabled);
+	
+	area=document.getElementById("txt");
+	
+	area.value=document.cookie;
+	
+	
+	
+});
+
+
+let save=function(){
+	document.cookie=encodeURIComponent(area.value);
+	console.log(document.cookie,area.value);
+};
+
+let load=function(){
+	area.value=document.cookie;
+};
